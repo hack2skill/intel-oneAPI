ea7914388e946693c60ed3dc7ef7720a2adaa814
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-22 22:26:43 +0530

    fastapi test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id, "name": "John Doe"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
