6aa3a7843f37fad31f94079b9bfa78e595e9245a
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-23 00:02:51 +0530

    updates

--- Backend/test.py
+++ Backend/test.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from fastapi import APIRouter
 
 app = FastAPI()
 
-@app.get("/")
+# Define a router for the user-related endpoints
+router_users = APIRouter()
+
+@router_users.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@router_users.get("/users")
+def get_users():
+    return {"users": ["Alice", "Bob", "Charlie"]}
+
+# Define a router for the item-related endpoints
+router_items = APIRouter()
+
+@router_items.get("/items")
+def get_items():
+    return {"items": ["item1", "item2", "item3"]}
+
+# Mount the routers on the app
+app.include_router(router_users)
+app.include_router(router_items)
 
 if __name__ == "__main__":
     import uvicorn
