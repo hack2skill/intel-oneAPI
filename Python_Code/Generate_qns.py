2d9230d9384dd48cc04583032e5c84cbb1db0e0e
Sameer-Abdulla <sameerabdwrk@gmail.com>
2023-05-22 23:46:50 +0530

    defined input parameters

--- Python_Code/Generate_qns.py
+++ Python_Code/Generate_qns.py
@@ -2,3 +2,14 @@ import openai
 
 # Set up your OpenAI API credentials
 openai.api_key = "##"
+
+
+# Define your input parameters
+with open('/Users/chemmi/Desktop/SRM-HACK/GenerateQns/input/mod2qns.txt', 'r') as file:
+    sorted_questions = str(file.read())
+with open('/Users/chemmi/Desktop/SRM-HACK/GenerateQns/input/notes_questions_list.txt', 'r') as file:
+    notebook_questions = str(file.read())    
+with open('/Users/chemmi/Desktop/SRM-HACK/GenerateQns/input/imp_topic_list.txt', 'r') as file:
+    important_topics = str(file.read())  
+with open('/Users/chemmi/Desktop/SRM-HACK/GenerateQns/input/referrence.txt', 'r', encoding='latin-1') as file:
+    reference_paper = str(file.read())            
