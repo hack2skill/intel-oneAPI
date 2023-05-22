8cbc0e12ec6734ebe224ec09cb64423fe710f1f9
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-22 23:54:50 +0530

    Update test.py

--- Backend/test.py
+++ Backend/test.py
@@ -6,14 +6,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/users")
-def get_users():
-    return {"users": ["Alice", "Bob", "Charlie"]}
-
-@app.get("/items")
-def get_items():
-    return {"items": ["item1", "item2", "item3"]}
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
