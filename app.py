20f4c296ccd56b69b293c0a8b8982f9d17ec916c
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-22 21:58:37 +0530

    testing flask api endpoint

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/api/example', methods=['GET'])
+def example_endpoint():
+    data = {'message': 'This is an example endpoint'}
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run()
