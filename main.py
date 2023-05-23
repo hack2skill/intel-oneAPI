dccd0d4ea8225dc7c61752b74f22ab827ecb4753
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-23 12:30:26 +0530

    angelo requirement.txt

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from Backend.pyqsorter import router as api1_router
-from Backend.summariser import router as api2_router
+#from Backend.summariser import router as api2_router
 # import other API routers as needed
 
 app = FastAPI()
-app1=FastAPI()
+#app1=FastAPI()
 
 # Mount the API routers
 app.include_router(api1_router)
-app1.include_router(api2_router)
+#app1.include_router(api2_router)
 # include other API routers as needed
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-    uvicorn.run(app1, host="0.0.0.0", port=8001)
+    #uvicorn.run(app1, host="0.0.0.0", port=8001)
