fcce207badf5738e4923814bb1ecfaf4e607f5e9
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-23 17:08:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
-from Backend.pyqsorter import router as api1_router
-from Backend.test import router_summariser as summariser
-from Backend.Notes_Analyser import router as api4_router
+#from Backend.pyqsorter import router as api1_router
+from Backend.summariser import router_summariser as summariser
+#from Backend.Notes_Analyser import router as api4_router
 # import other API routers as needed
 
 app = FastAPI()
 
 
-# Mount the API routers
-app.include_router(api1_router)
+# Mount the API routerss
+#app.include_router(api1_router)
 app.include_router(summariser)
-app.include_router(api4_router)
+#app.include_router(api4_router)
 
 # include other API routers as needed
 
