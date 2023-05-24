b332b34cb9dee66dc585e129372edad2e5af08a9
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-24 23:36:33 +0530

    debugging speech2txt

--- Backend/speech_text.py
+++ Backend/speech_text.py
@@ -13,8 +13,17 @@ speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
 def speech_to_text(audio_file):
     audio_config = speechsdk.audio.AudioConfig(filename=audio_file)
+    push_stream = speechsdk.audio.PushAudioInputStream()
 
-    result = speech_recognizer.recognize_once_async(audio_config).get()
+    with open(audio_file, "rb") as file:
+        data = file.read()
+        push_stream.write(data)
+
+    push_stream.close()
+
+    speech_recognizer.set_input(push_stream)
+    result = speech_recognizer.recognize_once()
+
     if result.reason == speechsdk.ResultReason.RecognizedSpeech:
         transcript = result.text
     else:
