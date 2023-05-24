736aca4ec9b30e62e41bb6d074f7a8e0baec66a7
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-24 22:57:10 +0530

    Update Speech_to_text.py

--- Backend/Speech_to_text.py
+++ Backend/Speech_to_text.py
@@ -1,5 +1,33 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-import speech_recognition as sr
-from google.cloud import speech_v1p1beta1 as speech
 import os
+import azure.cognitiveservices.speech as speechsdk
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI()
+
+speech_key = "<Your Azure Speech Subscription Key>"
+service_region = "<Your Azure Speech Region>"
+
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+def speech_to_text(audio_file):
+    audio_config = speechsdk.AudioConfig(filename=audio_file)
+
+    result = speech_recognizer.recognize_once_async(audio_config).get()
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        transcript = result.text
+    else:
+        transcript = "Speech recognition failed: {}".format(result.reason)
+
+    return transcript
+
+@app.post("/speech-to-text")
+async def convert_speech_to_text(file: UploadFile):
+    file_path = f"audio_files/{file.filename}"
+    os.makedirs("audio_files", exist_ok=True)
+    with open(file_path, "wb") as audio:
+        audio.write(await file.read())
+
+    transcript = speech_to_text(file_path)
+
+    return {"transcript": transcript}
