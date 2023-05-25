9154d4f1204a065727700787546a03f63cf27643
harshedabdulla <harshed2004@gmail.com>
2023-05-25 11:07:46 +0530

    upload prt

--- client/src/components/UploadNotes.jsx
+++ client/src/components/UploadNotes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import Lotte from 'lottie-react';
+import Lottie from 'lottie-react';
 import animationData from '../assets/95241-uploading.json';
 
 function UploadNotes() {
@@ -10,41 +10,44 @@ function UploadNotes() {
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
+    setUploadSuccess(false); // Reset upload success status when a new file is selected
   };
 
   const handleUpload = () => {
     if (!selectedFile) {
       return;
     }
-  
+
     setUploading(true);
-  
+
     const reader = new FileReader();
-  
+
     reader.onload = () => {
       // Get the file contents
       const fileContents = reader.result;
-  
+
       // Create a Blob from the file contents
       const blob = new Blob([fileContents], { type: selectedFile.type });
-  
+
       // Create a file path within the Local_Storage/notes_pdf folder
       const filePath = `Local_Storage/notes_pdf/${selectedFile.name}`;
-  
+
       // Save the file locally
       saveFileLocally(filePath, blob)
         .then(() => {
           setUploadSuccess(true);
-          setUploading(false);
         })
         .catch((error) => {
           console.error('Error saving file:', error);
+        })
+        .finally(() => {
+          setUploading(false);
         });
     };
-  
+
     reader.readAsArrayBuffer(selectedFile);
   };
-  
+
   const saveFileLocally = (filePath, file) => {
     return new Promise((resolve, reject) => {
       const virtualLink = document.createElement('a');
@@ -62,7 +65,6 @@ function UploadNotes() {
       document.body.removeChild(virtualLink);
     });
   };
-  
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
@@ -72,7 +74,7 @@ function UploadNotes() {
         animate={{ scale: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <Lotte animationData={animationData} style={{ width: 400, height: 300 }} />
+        <Lottie animationData={animationData} style={{ width: 400, height: 300 }} />
         <h1 className="text-3xl font-bold mb-4">Upload Notes</h1>
         {!uploadSuccess ? (
           <>
@@ -91,7 +93,7 @@ function UploadNotes() {
               whileHover={!uploading ? { scale: 1.05 } : {}}
               whileTap={!uploading ? { scale: 0.95 } : {}}
             >
-              {uploading ? 'Uploading...' : 'Upload'}
+              {uploading ? 'Uploaded' : 'Upload'}
             </motion.button>
           </>
         ) : (
@@ -99,7 +101,6 @@ function UploadNotes() {
             className="text-xl"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
           >
             Upload successful!
           </motion.div>
