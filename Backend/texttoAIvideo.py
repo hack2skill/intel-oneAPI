cd8317b81e1472a845f6696baf7e4a434c53057b
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-25 11:43:02 +0530

    3

--- Backend/texttoAIvideo.py
+++ Backend/texttoAIvideo.py
@@ -1,27 +1,19 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-router = APIRouter()
 
-@router.get('/api6')
+@app.get('/api6')
 async def get_text_from_file():
     with open('input.txt', 'r') as file:
         file_text = file.read(200)
     return file_text
 
 # Configure CORS
-origins = [
-    "http://localhost:3000",  # Replace with your frontend URL
-    # Add more allowed origins if needed
-]
-
-app.include_router(router)
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
