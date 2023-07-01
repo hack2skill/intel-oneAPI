6ae7d2f05cbac63e163dea4df69c9cd501de402c
R. Aditya <59119736+adityarags@users.noreply.github.com>
2023-07-02 02:51:36 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -5,15 +5,24 @@ app = Flask(__name__, template_folder = "templates")
 
 
 # Controllers
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
+def mainPage():
+    return render_template("index.html", page = "Home")
+
+@app.route("/predict", methods = ["GET", "POST"])
 def home():
     output = "hold"
     if request.method == "POST":
         f = request.files['file']
-        f.save("inputImage.jpg") 
+        f.save("static/inputImage.jpg")
         output = "show"
         predict()
-    return render_template("index.html", output = output)
+    return render_template("index.html", output = output, page = "Predict")
+
+
+@app.route("/team")
+def team():
+    return render_template("index.html", page = "Team")
 
 if __name__ == "__main__":
     app.run(debug = True)
